82bf758543c91a754661b43db4c0778660ad100b
Pablo-Panchig <pspanchig@gmail.com>
2024-02-08 16:43:00 -0600

    Saved

--- FronEnd/Login_src/LoginPage.jsx
+++ FronEnd/Login_src/LoginPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './LoginPage.css'
 import videoBackgrond from './assets/pexels-tima-miroshnichenko-6498240 (2160p).mp4'
 const LoginPage = () => {
+
+  const [goback] = ("index.html")
   return (
     <div className='LoginBox'>
 
@@ -9,27 +11,30 @@ const LoginPage = () => {
         <video autoPlay loop muted playsInline  src={videoBackgrond}></video>
       </div>
       <div className='loginForm'>
+        {/* <div>
+
+        </div> */}
         <h1>Sign in for current users</h1>
-      <form action="test.html">
-        <label htmlFor="">Username</label>
-        <input type="text" placeholder='Username'/>
-        <label htmlFor="">Passowrd</label>
-        <input type="Password" placeholder='Password'/>
-        <div className='formsbuttons'>
-          <input type='submit'/>
-        </div>        
+        <form action="test.html">
+          <label htmlFor="">Username</label>
+          <input type="text" placeholder='Username'/>
+          <label htmlFor="">Passowrd</label>
+          <input type="Password" placeholder='Password'/>
+
+          <div className='formsbuttons'>
+            <input type='submit'/>
+          </div>        
       </form> 
-      <div>
-          <ul>
-            <li>
-              <a href="">Create New account</a>
-            </li>
-            <li>
-              <a href="">Go Back</a>
-            </li>
-            
-          </ul>
-      </div>
+        <div>
+            <ul>
+              <li>
+                <a href="">Create New account</a>
+              </li>
+              <li>
+                <a href={goback}>Go Back</a>
+              </li>            
+            </ul>
+        </div>
       </div>      
     </div>
   )
