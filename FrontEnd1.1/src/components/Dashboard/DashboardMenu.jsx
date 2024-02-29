a25de463dab28cfb7a05cef11ebbc32ca4297191
Pablo-Panchig <pspanchig@gmail.com>
2024-02-28 19:19:07 -0600

    Code

--- FrontEnd1.1/src/components/Dashboard/DashboardMenu.jsx
+++ FrontEnd1.1/src/components/Dashboard/DashboardMenu.jsx
@@ -5,17 +5,42 @@ import './css/DashboardMenu.css'
 const DashboardMenu = () => {
 
     useEffect(() => {
-        const CreateAnyElement = () =>{
-            const container =  document.getElementById('DBC-Container');
-            const div = document.createElement('div')
-            const h1 = document.createElement('h1')
-            h1.innerHTML = '1';
-    
-            container.append(div)
-            div.append(h1)        
-        }
-        CreateAnyElement();
-    })
+        const bringDB = async () => {
+            const url = "http://localhost:8080/findAll";
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok: ' + response.statusText);
+                }
+                const data = await response.json();
+                console.log("Information: ", data);
+                return data; 
+            } catch (error) {
+                console.error("The problem is here: ", error);                
+                throw error;
+            }
+        };
+        
+        const CreateDBCard = async () => { // This function now needs to be async to use await
+            try {
+                let dbData = await bringDB(); // Wait for the data to be fetched
+        
+                const container = document.getElementById('DBC-Container');
+                const card = document.createElement('div');
+                card.classList.add('DBDataCard');
+                card.textContent = "SEXO"; 
+                container.append(card);
+                console.log(dbData);
+
+            } catch (error) {
+                console.error("Error while creating DB cards: ", error);
+            }
+        };
+            
+        document.addEventListener('DOMContentLoaded', CreateDBCard()); 
+
+    },[])
+
 
 
   return (
@@ -27,54 +52,6 @@ const DashboardMenu = () => {
         <div className='TodayData'>
             <h4>Today's Data</h4>
             <div className='DBDataCard-Container' id='DBC-Container'>
-                <div className='DBDataCard'>
-                    <p>Data base 1</p>
-                    <p>Employeers 1</p>
-                    <div className='DBDataCard-Status'>
-                        <p>Status</p>
-                        <p id='Online'>Online</p>
-                    </div>
-                </div>
-                <div className='DBDataCard'>
-                    <p>Data base 1</p>
-                    <p>Employeers 1</p>
-                    <div className='DBDataCard-Status'>
-                        <p>Status</p>
-                        <p id='Online'>Online</p>
-                    </div>
-                </div>
-                <div className='DBDataCard'>
-                    <p>Data base 1</p>
-                    <p>Employeers 1</p>
-                    <div className='DBDataCard-Status'>
-                        <p>Status</p>
-                        <p id='Online'>Online</p>
-                    </div>
-                </div>
-                <div className='DBDataCard'>
-                    <p>Data base 1</p>
-                    <p>Employeers 1</p>
-                    <div className='DBDataCard-Status'>
-                        <p>Status</p>
-                        <p id='Online'>Online</p>
-                    </div>
-                </div>
-                <div className='DBDataCard'>
-                    <p>Data base 1</p>
-                    <p>Employeers 1</p>
-                    <div className='DBDataCard-Status'>
-                        <p>Status</p>
-                        <p id='Online'>Online</p>
-                    </div>
-                </div>
-                <div className='DBDataCard'>
-                    <p>Data base 1</p>
-                    <p>Employeers 1</p>
-                    <div className='DBDataCard-Status'>
-                        <p>Status</p>
-                        <p id='Online'>Online</p>
-                    </div>
-                </div>
                 <div className='DBDataCard'>
                     <p>Data base 1</p>
                     <p>Employeers 1</p>
